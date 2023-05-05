f6dd49bbfa180b566733b74067e8faf8e855845d
ReneKubax <therene21@gmail.com>
2023-05-04 23:30:03 -0600

    Change of styles

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -4,7 +4,11 @@ import { FC, HTMLAttributes } from "react";
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement>{}
 
 const ChatInput: FC<ChatInputProps> = ({className, ...props}) => {
-    return <div {...props} className={cn('border-t border-zinc-300', className)}>Chat Input</div>
+    return <div {...props} className={cn('border-t border-zinc-300', className)}>
+       <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
+        
+       </div>
+    </div>
 }
 
 export default ChatInput
