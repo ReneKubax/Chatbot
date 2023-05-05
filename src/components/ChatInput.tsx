89fe98666c6c451fd54adfac6297f9d73eeeac67
ReneKubax <therene21@gmail.com>
2023-05-04 23:26:24 -0600

    Creation of chat input

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,9 +1,10 @@
-import { FC } from "react";
+import { cn } from "@/lib/utils";
+import { FC, HTMLAttributes } from "react";
 
-interface ChatInputProps {}
+interface ChatInputProps extends HTMLAttributes<HTMLDivElement>{}
 
-const ChatInput: FC<ChatInputProps> = () => {
-    return <div>Chat Input</div>
+const ChatInput: FC<ChatInputProps> = ({className, ...props}) => {
+    return <div {...props} className={cn('border-t border-zinc-300', className)}>Chat Input</div>
 }
 
 export default ChatInput
