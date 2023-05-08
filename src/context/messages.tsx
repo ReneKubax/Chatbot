934a2657f111f46d54ee0b16175d8c374d922daa
ReneKubax <therene21@gmail.com>
2023-05-08 16:10:49 -0600

    complete context component

--- src/context/messages.tsx
+++ src/context/messages.tsx
@@ -1,5 +1,6 @@
 import { Message } from "@/lib/validators/message";
-import { createContext } from "react";
+import { nanoid } from "nanoid";
+import { ReactNode, createContext, useState } from "react";
 
 export const MessagesContext = createContext<{
   messages: Message[]
@@ -16,3 +17,42 @@ export const MessagesContext = createContext<{
   updateMessage: () => {},
   setIsMessageUpdating: () => {},
 })
+
+export function MessagesProvider({children}: {children: ReactNode}) {
+
+    const [isMessageUpdating, setIsMessageUpdating] = useState<boolean>(false)
+
+    const [messages, setMessages] = useState<Message[]>([
+        {
+            id: nanoid(),
+            isUserMessage: true,
+            text: 'Hello, how can i help you?'
+        }
+    ])
+
+    const addMessage = (message: Message) => {
+        setMessages((prev)=> [...prev, message])
+    }
+
+    const removeMessage = (id: string) => {
+        setMessages((prev)=> prev.filter((message) => message.id !== id))
+    }
+
+    const updateMessage = (id: string, updateFn: (prevText: string) => string) => {
+        setMessages((prev)=> prev.map((message) => message.id === id ? {...message, text: updateFn(message.text)} : message))
+    }
+
+
+  return (
+    <MessagesContext.Provider value={{
+      messages,
+      addMessage,
+      removeMessage,
+      updateMessage,
+      isMessageUpdating,
+      setIsMessageUpdating
+    }}>
+      {children}
+    </MessagesContext.Provider>
+  )
+}
