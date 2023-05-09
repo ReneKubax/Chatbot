681735f159902cdede7d0ec1b756ee3f0e4b4bc2
ReneKubax <therene21@gmail.com>
2023-05-08 22:54:44 -0600

    classes of components

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import { MessagesContext } from "@/context/messages";
+import { cn } from "@/lib/utils";
 import { FC, HTMLAttributes, useContext } from "react";
 
 interface ChatMessagesProps extends HTMLAttributes<HTMLDivElement>{}
 
 const ChatMessages: FC<ChatMessagesProps> = ({className, ...props}) => {
     const {messages} = useContext(MessagesContext)
-    return <div {...props}>ChatMessages</div>
+    const inverseMessages = [...messages].reverse()
+
+
+
+    return <div {...props} className={cn("flex flex-col-reverse gap-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch",
+     className)}>
+        ChatMessages</div>
 }
 
 export default ChatMessages
