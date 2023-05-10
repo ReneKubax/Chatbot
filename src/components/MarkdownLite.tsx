3388ee9b79747e235fde53842a511d40a0af9ce3
ReneKubax <therene21@gmail.com>
2023-05-09 23:02:31 -0600

    creating logic of markdown component

--- src/components/MarkdownLite.tsx
+++ src/components/MarkdownLite.tsx
@@ -6,6 +6,21 @@ interface MarkdownLiteProps {
 
 const MarkdownLite: FC<MarkdownLiteProps> = ({text}) => {
     const linkRegex = /\[(.+?)\]\((.+?)\)/g
+    const parts = []
+
+    let lastIndex = 0
+    let match
+
+    while ((match = linkRegex.exec(text)) !== null) {
+        const [fullMatch, linkText, linkUrl] = match
+        const matchStart = match.index
+        const matchEnd = matchStart + fullMatch.length
+        
+        if(lastIndex < matchStart) {
+            parts.push(text.slice(lastIndex, matchStart))
+        }
+    }
+    
     return <div>MarkdownLite</div>;
 }
 
