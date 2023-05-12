87809392c77d65e787872470e0a3f2918cbfa444
ReneKubax <therene21@gmail.com>
2023-05-11 21:51:14 -0600

    changes on providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,14 +4,18 @@ import { MessagesProvider } from "@/context/messages"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import {FC, ReactNode} from "react"
 
-interface ProvidersProps {
+interface LayoutProps {
     children: ReactNode
-}
-
-    const Providers: FC<ProvidersProps>=({children})=>{
-        const queryClient = new QueryClient()
-        return <QueryClientProvider client={queryClient}><MessagesProvider>{children}</MessagesProvider></QueryClientProvider>
-    }
-
-
-export default Providers
+  }
+  
+  const Layout: FC<LayoutProps> = ({ children }) => {
+    const queryClient = new QueryClient()
+  
+    return (
+      <QueryClientProvider client={queryClient}>
+        <MessagesProvider>{children}</MessagesProvider>
+      </QueryClientProvider>
+    )
+  }
+  
+  export default Layout
