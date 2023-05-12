06f3ecd6ca1093638e9c945960ccb35cb71a73bc
ReneKubax <therene21@gmail.com>
2023-05-11 21:47:17 -0600

    page status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-   <main className='absolute inset-0 flex justify-center items-center'>Website content</main>
+    <main className='absolute inset-0 flex justify-center items-center'>
+      <div style={{ marginBottom: '5rem' }}>
+        Hello, currently the chat bot is offline due to lack of budget in the Open Ia API. 
+        If you want the application to be online you can support me in the following text :)
+      </div>
+      <Link href="https://paypal.me/renefuenteskbx?country.x=GT&locale.x=es_XC" legacyBehavior style={{ marginTop: '1rem' }}>
+        <a>Support me</a>
+      </Link>
+    </main>
+    
   )
 }
